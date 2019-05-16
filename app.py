4cc0fddda7bfedd1e0bf8ffa64a177dcb51ddfbe
Zach Gittelman <zachgitt@umich.edu>
2019-05-15 23:55:34 -0400

    completed initial selection, going to work on reusable templates

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return render_template('base.html')
+	return render_template('select.html')
+
+@app.route('/list', methods=['POST'])
+def list():
+	return render_template('list.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
