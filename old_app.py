594e6d0724b2a38fd263d06967e3189740c68e66
Zach Gittelman <zachgitt@umich.edu>
2019-05-16 12:50:44 -0400

    commiting this mess, about to clean up because selection working now

--- old_app.py
+++ old_app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, abort
 app = Flask(__name__)
 
+#from helpers import get_unique_provider_names, get_unique_operatory_names
 
 @app.route('/')
 def main():
 	return render_template('select.html')
 
 @app.route('/list', methods=['POST'])
-def list():
+def h():
 
 	# parse input
 	selection = request.form.get('selection')
@@ -15,15 +16,19 @@ def list():
 
 	# validate inputs
 	if selection not in ['providers', 'operatories']:
-		abort(400, 'Please select providers or operatories to search!')
-	if (length % 5 != 0) or (length < 5) or (60 < length):
-		abort(400, 'Uh oh, the schedule length must be between 5 and 60 and a 5 minute interval.')
+		abort(404)
+	if (length % 5) != 0 and (5 <= length <= 60):
+		abort(404)		
 
 	# display list of names
-	names = ['a', 'wanda']
-	
+    names = []	
+	#if selection == 'providers':
+	#	names = get_unique_provider_names()	
+	#else:
+	#	names = get_unique_operatory_names() 
 
-	return render_template('list.html', names=names)
+	return render_template('list.html', selection=selection)
+	#return render_template('list.html', selection=selection.capitalize(), names=names)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
